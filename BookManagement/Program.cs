18ef57655f674a1ee056494b664e92f7f6e61675
MSI <MSI@DESKTOP-GRH9LP9>
2018-01-13 19:40:52 +0700

    ...

--- BookManagement/Program.cs
+++ BookManagement/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BookManagement.Forms;
+using BookManagementBUS;
+using BookManagementDAL;
 namespace BookManagement
 {
     static class Program
